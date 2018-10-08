ff88cf9e37e3ac70d244c2cd68622d2d19d34b25
Walinns-Manivasagam <43805862+Walinns-Manivasagam@users.noreply.github.com>
2018-10-08 15:51:33 +0530

    Updated

--- www/walinnshybridapi.js
+++ www/walinnshybridapi.js
@@ -1,13 +1,13 @@
 var exec = require('cordova/exec');
 
 exports.coolMethod = function (arg0, arg1, success, error) {
-    exec(success, error, 'WalinnsHybridPlugin', 'coolMethod', [arg0, arg1]);
+    exec(success, error, 'walinnshybridapi', 'coolMethod', [arg0, arg1]);
 };
 
 exports.trackEvent = function (type, eventName, success, error) {
-    exec(success, error, 'WalinnsHybridPlugin', 'trackEvent', [type, eventName]);
+    exec(success, error, 'walinnshybridapi', 'trackEvent', [type, eventName]);
 };
 
 exports.trackScreen = function (screenName, success, error) {
-    exec(success, error, 'WalinnsHybridPlugin', 'trackScreen', [screenName]);
+    exec(success, error, 'walinnshybridapi', 'trackScreen', [screenName]);
 };
